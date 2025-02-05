6e818aa4c87ea7374a8850c2e6c3b133ae159336
Yulia Kuznetsova <kooznitsa@gmail.com>
2025-02-05 11:46:27 +0300

    [fix] fix line

--- common/validators.py
+++ common/validators.py
@@ -4,6 +4,7 @@ from typing import NoReturn
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+
 def validate_duration(value: str) -> NoReturn:
     if not re.match(r'^[1-9]\d?:\d{2}$', value):
         raise ValidationError(
